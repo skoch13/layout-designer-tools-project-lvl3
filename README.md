63533de83b5c07cb1187e67f7ecd04c9d257107b
Sergey Kochetkov <sergey.kochetkov@jetbrains.com>
2022-05-23 21:07:33 +0200

    Revert "fixes"
    This reverts commit b43d77ff

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ### Hexlet tests and linter status:
+[![Actions Status](https://github.com/skoch13/layout-designer-tools-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/skoch13/layout-designer-tools-project-lvl3/actions)
 
-[![Actions Status](https://github.com/agapovk/layout-designer-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/agapovk/layout-designer-project-lvl3/actions)
-
-[Ссылка на surge:](https://anxious-middle.surge.sh)
+https://noisy-bedroom.surge.sh
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,58 @@
-const { src, dest, parallel, series } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
 
 const browserSyncJob = () => {
   browserSync.init({
     server: "build/"
   });
 
-  watch('app/scss/*.scss', buildSass);
-  watch('app/*.pug', buildPug);
+  watch('app/scss/**/*.scss', buildSass);
+  watch('app/pages/**/*.pug', buildPug);
 };
 
 const buildSass = () => {
-  console.log('Компиляция SASS');
+  console.log('Compiling SASS');
 
-  return src('app/scss/*.scss')
+  return src('app/scss/main.scss')
     .pipe(sass())
     .pipe(dest('build/styles/'))
     .pipe(browserSync.stream());
 }
 
 const buildPug = () => {
-  console.log('Компиляция Pug');
+  console.log('Compiling Pug');
 
-  return src('app/*.pug')
-    .pipe(pug(
-      {pretty: true}
-    ))
+  return src('app/pages/*.pug')
+    .pipe(pug())
     .pipe(dest('build/'))
     .pipe(browserSync.stream());
 }
 
+const copyImages = () => {
+  console.log('Copying images');
+
+  return src('app/images/*.jpg').pipe(dest('build/images'))
+}
+
+const prepareSvg = () => {
+  console.log('Preparing SVGs');
+
+  const config = {
+    mode: {
+      symbol: {
+        sprite: 'sprite.svg'
+      }
+    }
+  }
+
+  return src('app/images/icons/*.svg').pipe(svgSprite(config)).pipe(dest('build/images'))
+}
+
+const build = parallel(buildSass, buildPug, copyImages, prepareSvg);
+
 exports.server = browserSyncJob;
-exports.build = parallel(buildSass, buildPug);
-exports.go = series(build, server);
+exports.build = build;
+exports.dev = series(build, browserSyncJob);
