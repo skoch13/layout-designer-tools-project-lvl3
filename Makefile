1e6bf44500c6a89371e6241106c0e5bc56fc2fad
Sergey Kochetkov <sergey.kochetkov@jetbrains.com>
2022-05-23 21:20:09 +0200

    Revert "fixes"
    This reverts commit d0450fb2825849567d64dd0b5baef9e30a4e4627.

--- Makefile
+++ Makefile
@@ -2,27 +2,7 @@ install:
 	npm install
 
 lint:
-	npx stylelint ./app/scss/**/*.scss
-	pug-lint ./app/pug/**/*.pug
-	npx htmlhint ./build/*.html
-
-pug-index:
-	pug ./app/pug/pages/index.pug --pretty -o ./build/
-
-pug-chat:
-	pug ./app/pug/pages/chat.pug --pretty -o ./build/
-
-pug:
-	make pug-index
-	make pug-chat
-
-sass:
-	sass ./app/scss/custom.scss ./build/css/style.css
-	npx stylelint --fix ./build/css/style.css
-
-build-pug-sass:
-	make pug
-	make sass
-
+	npx stylelint ./src/styles/*.css
+	npx stylelint ./src/styles/**/*.scss
 deploy:
-	npx surge ./build/
+	npx surge ./src/
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,58 @@
-const gulp = require('gulp');
-const images = require('./gulp/images');
-const spriteSVG = require('./gulp/spriteSVG');
-const pug2html = require('./gulp/pug2html');
-const styles = require('./gulp/styles');
-const scripts = require('./gulp/scripts');
-const clean = require('./gulp/clean');
-const serve = require('./gulp/serve');
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
 
-const { series, parallel } = gulp;
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/"
+  });
 
-const dev = parallel(images, spriteSVG, pug2html, styles, scripts);
+  watch('app/scss/**/*.scss', buildSass);
+  watch('app/pages/**/*.pug', buildPug);
+};
 
-exports.images = images;
-exports.spriteSVG = spriteSVG;
-exports.pug2html = pug2html;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.clean = clean;
-exports.serve = serve;
+const buildSass = () => {
+  console.log('Compiling SASS');
 
-exports.default = series(clean, dev, serve);
+  return src('app/scss/main.scss')
+    .pipe(sass())
+    .pipe(dest('build/styles/'))
+    .pipe(browserSync.stream());
+}
+
+const buildPug = () => {
+  console.log('Compiling Pug');
+
+  return src('app/pages/*.pug')
+    .pipe(pug())
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
+}
+
+const copyImages = () => {
+  console.log('Copying images');
+
+  return src('app/images/*.jpg').pipe(dest('build/images'))
+}
+
+const prepareSvg = () => {
+  console.log('Preparing SVGs');
+
+  const config = {
+    mode: {
+      symbol: {
+        sprite: 'sprite.svg'
+      }
+    }
+  }
+
+  return src('app/images/icons/*.svg').pipe(svgSprite(config)).pipe(dest('build/images'))
+}
+
+const build = parallel(buildSass, buildPug, copyImages, prepareSvg);
+
+exports.server = browserSyncJob;
+exports.build = build;
+exports.dev = series(build, browserSyncJob);
